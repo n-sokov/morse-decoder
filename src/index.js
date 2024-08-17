c393e386ea930e1a87e4d85673395b99447df413
Nikita Sokov <nikitasokov1@gmail.com>
2024-08-17 12:18:43 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -39,6 +39,28 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const exprToString = `${expr}`;
+    let result = "";
+    for (let i = 1; i <= exprToString.length / 10; i++) {
+        let tmp = "";
+        for (let j = 10 * (i - 1); j < 10 * i - 1; j += 2) {
+            if (exprToString[j] === "*") {
+                tmp += " ";
+                break;
+            }
+            if (exprToString[j] === "1" && exprToString[j + 1] === "0") {
+                tmp += ".";
+            } else if (exprToString[j] === "1" && exprToString[j + 1] === "1") {
+                tmp += "-";
+            }
+        }
+        if (tmp === " ") {
+            result += " ";
+            continue;
+        }
+        result += MORSE_TABLE[tmp];
+    }
+    return result;
 }
 
 module.exports = {
